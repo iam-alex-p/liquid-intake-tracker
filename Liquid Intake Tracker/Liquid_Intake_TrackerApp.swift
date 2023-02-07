bb5b65b97c39a69e6aa3b917326821b24c367618
Alex P <alexey.pokolev@gmail.com>
2023-02-06 18:02:37 -0800

    Initial Commit

---
+++ Liquid Intake Tracker/Liquid_Intake_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Liquid_Intake_TrackerApp.swift
+//  Liquid Intake Tracker
+//
+//  Created by Aleksei Pokolev on 2/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Liquid_Intake_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
